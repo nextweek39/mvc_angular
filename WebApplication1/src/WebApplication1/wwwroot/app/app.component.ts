ada593d47e4b98d0c54c675fdd9af8c7ae694769
nextweek <maartri@gmail.com>
2016-04-11 16:31:50 +0800

    first commit

---
+++ WebApplication1/src/WebApplication1/wwwroot/app/app.component.ts
@@ -0,0 +1,27 @@
+﻿import {Component, OnInit} from "angular2/core";
+import {RouteConfig, ROUTER_DIRECTIVES, RouterLink, RouteDefinition} from "angular2/router";
+
+
+import {StaticComponent} from "./components/static.component";
+import {StaffComponent} from "./components/staff.component";
+
+
+@RouteConfig([
+    { path: "/", component: StaticComponent, name: "Home", useAsDefault: true },
+    { path: "/staff", component: StaffComponent, name: "Staff" }
+])
+
+@Component({
+    selector: "app",
+    templateUrl: "app/app.html",
+    directives: [ROUTER_DIRECTIVES, StaticComponent, StaffComponent, RouterLink]
+})
+
+
+
+export class AppComponent {
+
+
+
+
+}
---
+++ WebApplication1/src/WebApplication1/wwwroot/app/components/staff.component.ts
@@ -0,0 +1,17 @@
+﻿import {Component} from 'angular2/core';
+import {ControlGroup, Control, FormBuilder, Validators} from 'angular2/common';
+
+
+@Component({
+    templateUrl: "app/components/staff.component.html",
+})
+
+export class StaffComponent {
+    addUser: ControlGroup;
+
+    constructor(fb: FormBuilder) {
+        this.addUser = fb.group({
+            name: ['', Validators.required]
+        })
+    }
+}
